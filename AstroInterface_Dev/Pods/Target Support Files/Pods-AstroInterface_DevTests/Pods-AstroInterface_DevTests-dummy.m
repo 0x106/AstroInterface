b07811a2b733181023556458f9b499ea6791809e
Jordan Campbell <jordan.evan.campbell@gmail.com>
2019-02-16 11:19:23 +1300

    created initial dev project

---
+++ AstroInterface_Dev/Pods/Target Support Files/Pods-AstroInterface_DevTests/Pods-AstroInterface_DevTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AstroInterface_DevTests : NSObject
+@end
+@implementation PodsDummy_Pods_AstroInterface_DevTests
+@end
---
+++ AstroInterface_Dev/Pods/Target Support Files/Pods-AstroInterface_DevUITests/Pods-AstroInterface_DevUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AstroInterface_DevUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AstroInterface_DevUITests
+@end
